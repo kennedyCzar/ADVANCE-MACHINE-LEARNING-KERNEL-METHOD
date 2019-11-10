1e1ecf6618ef2b1a55b7cced890f797fa85bd2a3
kennedy <kennyjunior022006@yahoo.com>
2019-11-10 06:11:30 +0100

    kernel Kmeans
    kernel Kmeans

--- KMEANS/KERNEL KMEANS/kernelkmeans.py
+++ KMEANS/KERNEL KMEANS/kernelkmeans.py
@@ -9,17 +9,33 @@ Created on Mon Oct 14 01:41:45 2019
 from __future__ import absolute_import
 import numpy as np
 from Utils.kernels import Kernels
+from Utils.utils import EvalC
 
-class kkmeans(Kernels):
-    def __init__(self, k = None,kernel = None):
+class kkMeans(EvalC, Kernels):
+    def __init__(self, k = None, kernel = None, gamma = None, d = None):
+        '''
+        :param: k: number of clusters
+        :param: kernel
+        
+        '''
         super().__init__()
+        if not gamma:
+            gamma = 5
+            self.gamma = gamma
+        else:
+            self.gamma = gamma
+        if not d:
+            d = 3
+            self.d = d
+        else:
+            self.d = d
         if not k:
-            k = 3
+            k = 2
             self.k = k
         else:
             self.k = k
         if not kernel:
-            kernel = 'linear'
+            kernel = 'rbf'
             self.kernel = kernel
         else:
             self.kernel = kernel
@@ -27,72 +43,108 @@ class kkmeans(Kernels):
     
     def kernelize(self, x1, x2):
         '''
-        :params: x1: NxD
-        :params: x2: NxD
+        :params: X: NxD
         '''
         if self.kernel == 'linear':
             return Kernels.linear(x1, x2)
         elif self.kernel == 'rbf':
-            return Kernels.rbf(x1, x2)
+            return Kernels.rbf(x1, x2, gamma = self.gamma)
         elif self.kernel == 'sigmoid':
-            return Kernels.sigmoid(x1, x2)
+            return Kernels.sigmoid(x1, x2, gamma = self.gamma)
         elif self.kernel == 'polynomial':
-            return Kernels.polynomial(x1, x2)
+            return Kernels.polynomial(x1, x2, d = self.d)
         elif self.kernel == 'cosine':
             return Kernels.cosine(x1, x2)
         elif self.kernel == 'correlation':
-            return Kernels.correlation(x1, x2)
+            return Kernels.correlation(x1, x2, gamma = self.gamma)
+        elif self.kernel == 'linrbf':
+            return Kernels.linrbf(x1, x2, gamma = self.gamma)
+        elif self.kernel == 'rbfpoly':
+            return Kernels.rbfpoly(x1, x2, d = self.d, gamma = self.gamma)
+        elif self.kernel == 'rbfcosine':
+            return Kernels.rbfpoly(x1, x2, d = self.d, gamma = self.gamma)
+        elif self.kernel == 'etakernel':
+            return Kernels.etakernel(x1, x2, d = self.d, gamma = self.gamma)
+        elif self.kernel == 'alignment':
+            return Kernels.alignment(x1, x2)
+        elif self.kernel == 'laplace':
+            return Kernels.laplacian(x1, x2, gamma = self.gamma)
+        elif self.kernel == 'locguass':
+            return Kernels.locguass(x1, x2, d = self.d, gamma = self.gamma)
+        elif self.kernel == 'chi':
+            return Kernels.chi(x1)
+    
+    #compute suqared kernel distance
+    def distance(self, kappa):
+        self.dist_w = np.zeros(self.k)
+        c_k_init = np.ones(kappa.shape[0])
+        for ii in range(self.k):
+            self.c_k_indices = self.clusters == ii
+            self.c_k = np.sum(c_k_init[self.c_k_indices])
+            self.c_k_squared = np.square(self.c_k)
+            self.kappa_ii = kappa[self.c_k_indices][:, self.c_k_indices]
+            self.distance_ii[:, ii] += np.sum(self.kappa_ii)/self.c_k_squared - 2*\
+                                    np.sum(kappa[:, self.c_k_indices], axis = 1)/self.c_k
+        return self.distance_ii
+           
+    #fit and return cluster labels
+    def fit_predict(self, X, iteration = None, halt = None):
+        '''
+        :param: X: NxD Feature
+        :param: iteration: 100
+        :param: tolerance: 1e-3 default
         
-    def distance(self, x, nu, axs = 1):
         '''
-        :param: x: datapoint
-        :param: nu: mean
-        :retrun distance matrix
-        '''
-       
-        return np.linalg.norm(x - nu, axis = axs)
-    
-    
-    def fit(self, X, iteration = None):
-        '''
-        :param: X: NxD
-        '''
+        if not halt:
+            halt = 1e-3
+            self.halt = halt
+        else:
+            self.halt = halt
         if not iteration:
-            iteration = 20
+            iteration = 100
             self.iteration = iteration
         else:
-            self.iteration = iteration
+            self.iteration
+        self.X = X
         N, D = X.shape
-        self.cluster = np.random.randint(low = 0, high = 2, size = N)
-        self.cost_rec = np.zeros(self.iteration)
-        for self.iter in range(self.iteration):
-            self.kappar = np.tile(self.kernelize(X, X).diagonal().reshape((-1, 1)), self.k)
-            self.z_i = np.bincount(self.cluster)
-            for self.c in range(self.k):
-                self.kappar[:, self.c] = self.kappar[:, self.c] + np.sum((self.kernelize(X, X))[self.cluster == self.c][:, self.cluster == self.c])/\
-                                            (self.z_i[self.c]**2) - 2*np.sum(self.kernelize(X, X)[:, self.cluster == self.c], axis = 1)/self.z_i[self.c]
+        self.distance_ii = np.zeros((N, self.k))
+        self.kx = self.kernelize(self.X, self.X)
+        self.clusters = np.random.randint(self.k, size = N)
+        '''iterate by checking to see if new and previous cluster
+        labels are similar. If they are, algorithm converges and halts..
+        '''
+        for ii in range(self.iteration):
+            #compute distance
+            self.distance_k = self.distance(self.kx)
+            self.prev_clusters = self.clusters
+            self.clusters = self.distance_k.argmin(axis=1)
+            if 1 - float(np.sum((self.clusters - self.prev_clusters) == 0)) / N < self.halt:
+                break
         return self
-    
-    def predict(self):
+
+    def rand_index_score(self, clusters, classes):
+        '''Compute the RandIndex
+        :param: Clusters: Cluster labels
+        :param: classses: Actual class
+        :returntype: float
         '''
-        :param: X: NxD
-        :return type: labels
-        '''
-        
-        return np.argmin(self.kappar, axis = 1)
+        from scipy.special import comb
+        tp_fp = comb(np.bincount(clusters), 2).sum()
+        tp_fn = comb(np.bincount(classes), 2).sum()
+        A = np.c_[(clusters, classes)]
+        tp = sum(comb(np.bincount(A[A[:, 0] == i, 1]), 2).sum()
+                 for i in set(clusters))
+        fp = tp_fp - tp
+        fn = tp_fn - tp
+        tn = comb(len(A), 2) - tp - fp - fn
+        self.randindex = (tp + tn) / (tp + fp + fn + tn)
+        return self.randindex
     
-
-#%% Testing
-from sklearn.datasets import make_circles, make_blobs
-X, y = make_blobs(1000, )
-import matplotlib.pyplot as plt  
-
-trans = [[.6, -.6], [-.4, .8]]
-X = X.dot(trans)
-kernelkmns = kkmeans(kernel='polynomial').fit(X)
-
-plt.scatter(X[:, 0], X[:, 1], c = kernelkmns.predict())
-
-
-
+##%% Testing
+#from sklearn.datasets import make_circles, make_moons, load_iris
+#X, y = load_iris().data, load_iris().target
+#X, y = make_moons(n_samples=samples, noise=.05)
+#X, y = make_circles(n_samples = 1000, noise = .10, factor=.05)
+#kkmeans = kkMeans(k = 3, kernel = 'linear', gamma=1).fit_predict(X)
+#plt.scatter(X[:, 0], X[:, 1], c = kkmeans.clusters)
 
